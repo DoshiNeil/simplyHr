348af915177c78fbefd45699508261389eea21ac
Neil Doshi <neil@nzymtechnologies.com>
2023-11-17 17:25:25 +0530

    dockerize api with postgres db

---
+++ api/src/index.ts
@@ -0,0 +1,33 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server, now dockerize soon");
+});
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432"),
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
